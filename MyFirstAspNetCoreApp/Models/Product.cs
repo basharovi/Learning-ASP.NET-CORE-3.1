d4ac97a55a36eeb9237d827482d6ce2bfb8ce315
Bashar Ovi <hello@basharovi.tech>
2020-04-06 23:31:16 +0600

    Ädded Product Model

---
+++ MyFirstAspNetCoreApp/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace MyFirstAspNetCoreApp.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Maker  { get; set; }
+
+        [JsonPropertyName("img")]
+        public string Image { get; set; }
+        public string Url { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int[] Ratings { get; set; }
+
+        public override string ToString() => JsonSerializer.Serialize(this);
+    }
+}
